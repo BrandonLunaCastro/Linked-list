1dd60f58283d3db78176a784c61de9f7c084aeb2
brandon <blunacastro2003@gmail.com>
2023-10-06 11:35:58 -0300

    fixed: add class LinkedList and node

---
+++ linked-List.js
@@ -0,0 +1,14 @@
+class LinkedList {
+    append(value){
+        return value = new node(value)
+    }
+    prepend(value){
+        return value = new node(value)
+    }
+}
+class node {
+    constructor(value = null, nextNode= null) {
+        this.value = value;
+        this.nextNode = nextNode
+    }
+}
