b9ff0673ca259fe2ef816196dd3e81af3b496ba9
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-10-06 18:11:57 -0300

    fixed: add functionality to functions prepend and append

--- linked-List.js
+++ linked-List.js
@@ -1,15 +1,38 @@
 class LinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+    head(){
+        return this.head;
+    }
     append(value){
-        return value = new node(value);
+        let node = new Node(value);
+        if(this.head === null){
+            this.head = node;
+        }else{
+            this.tail.nextNode = node;
+        }
+        this.tail = node;
     }
     prepend(value){
-        return value = new node(value);
+        let node = new Node(value);
+        node.nextNode = this.head.nextNode
+        this.head.nextNode = node;
+    }
+    toString(){
+        return console.log();
     }
 }
-class node {
+class Node {
     constructor(value = null, nextNode= null) {
         this.value = value;
-        this.nextNode = nextNode
-    }
+        this.nextNode = nextNode;
+    };
 }
-
+const listOne = new LinkedList()
+listOne.append("one");
+listOne.append("two");
+listOne.append("three");
+listOne.prepend(2)
+console.log(listOne);
